aa20b7496cf55b530f8a9ad708cae8657dc0bbbf
yegorf1 <yegorf1@gmail.com>
2018-08-22 01:01:41 +0300

    Add Second lab model
    Signed-off-by: yegorf1 <yegorf1@gmail.com>

---
+++ Models/Labs/SecondLabModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ComputedMath.Solvers.SecondLab;
+using Irony.Parsing;
+
+namespace ComputedMath.Models.Labs {
+    public class SecondLabModel : LabResultsModel {
+        public override string Name => "Integral approximation";
+
+        public string Function { get; set; } = "x ** 2";
+        public double A { get; set; } = 0;
+        public double B { get; set; } = 10;
+        public double Precision { get; set; } = 0.001;
+        public string Method { get; set; }
+        public string Subtype { get; set; }
+
+        public override void Solve() {
+            base.Solve();
+
+            var expressionGrammar = new LabGrammar();
+            var language = new LanguageData(expressionGrammar);
+            var parser = new Parser(language);
+            var runtime = new LabRuntime(language);
+
+            ParseTree tree = parser.Parse(Function);
+
+            if (tree.HasErrors()) {
+                Results.Add(new BoxModel("Error at parsing", "Syntax errors found. Abrting."));
+                return;
+            }
+
+            Results.AddRange(IntegralCalculator.Calculate(
+                tree,
+                runtime,
+                A,
+                B,
+                Precision,
+                Method,
+                Subtype
+            ));
+        }
+    }
+}
