1d4b4d7c6791d31f5129033c3758c817c469ffa8
yegorf1 <yegorf1@gmail.com>
2018-08-23 20:22:38 +0300

    Implemented Newton forulas

--- Solvers/ThirdLab/Interpolation.cs
+++ Solvers/ThirdLab/Interpolation.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ComputedMath.Solvers.ThirdLab {
     public static class Interpolation {
+        public delegate Func<double, double> GenerateApproximatedPolynom(double[] xData, double[] yData);
+        
         public static Func<double, double> LaGrangePolynom(double[] xData, double[] yData) {
             if (xData.Length != yData.Length) {
                 throw new ArgumentException("xData length must be equable to yData's");
@@ -19,12 +22,52 @@ namespace ComputedMath.Solvers.ThirdLab {
                 if (i == j) {
                     continue;
                 }
-                    
+
                 denominator *= xData[i] - xData[j];
                 numerator *= x - xData[j];
             }
 
             return numerator / denominator;
+        }
+
+        public static Func<double, double> NewtonFomula(double[] xData, double[] yData) {
+            return (x) => {
+                if (x < xData[0]) {
+                    return 0;
+                }
+
+                int zeroIndex = xData.Count(x0 => x0 <= x) - 1;
+                double q = (x - xData[zeroIndex]) / (xData[1] - xData[0]);
+                var skippedYData = new double[yData.Length - zeroIndex];
+                Array.Copy(yData, zeroIndex, skippedYData, 0, skippedYData.Length);
+
+                int n = Math.Min(yData.Length - zeroIndex - 1, 5);
+    
+                double result = yData[zeroIndex];
+                for (var i = 0; i < n; i++) {
+                    double qs = 1;
+                    for (var j = 0; j <= i; j++) {
+                        qs *= q - j;
+                    }
+
+                    double fact = 1;
+                    for (var j = 2; j <= i + 1; j++) {
+                        fact *= j;
+                    }
+
+                    result += qs * FiniteDiffirences(yData, zeroIndex, i + 1) / fact;
+                }
+
+                return result;
+            };
+        }
+
+        private static double FiniteDiffirences(IReadOnlyList<double> yData, int k, int n) {
+            if (n <= 0) {
+                return yData[k];
+            }
+
+            return FiniteDiffirences(yData, k + 1, n - 1) - FiniteDiffirences(yData, k, n - 1);
         }
     }
 }
